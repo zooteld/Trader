365fbfdbbae6871b2cb22eab3fa2365a84da51af
zooteld <zooteld@gmail.com>
2019-05-25 21:41:03 -0500

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -18,7 +18,7 @@ namespace parameters {
 const uint64_t CRYPTONOTE_MAX_BLOCK_NUMBER													= 500000000;
 const size_t   CRYPTONOTE_MAX_BLOCK_BLOB_SIZE												= 500000000;
 const size_t   CRYPTONOTE_MAX_TX_SIZE														= 1000000000;
-const uint64_t CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX										= 0x7ad4; /* w2w7 address prefix */
+const uint64_t CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX										= 0x44ad; /* w2w7 address prefix */
 const size_t   CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW											= 10; /* 20 minutes */
 const uint64_t CRYPTONOTE_BLOCK_FUTURE_TIME_LIMIT											= 60 * 60 * 2; /* two hours */
 const uint64_t CRYPTONOTE_BLOCK_FUTURE_TIME_LIMIT_V1										= 360; /* changed for LWMA3 */

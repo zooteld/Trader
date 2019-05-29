4c05813618ef8049ddb41ad4238042a081afa174
zooteld <zooteld@gmail.com>
2019-05-28 22:21:22 -0500

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -122,7 +122,7 @@ const uint64_t MAX_BLOCK_REWARD					= (UINT64_C(20) * parameters::COIN); // max 
 const uint64_t REWARD_INCREASE_INTERVAL				= (UINT64_C(21900)); // aprox. 1 month (+ 0.25 CCX increment per month)
 
 const char     CRYPTONOTE_NAME[]                             	= "W2W";
-const char     GENESIS_COINBASE_TX_HEX[]			= "010a01ff0001c096b102029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101305f1b9b6940d6fa793e327d9ba3b476cdd2e4f338cdda8b87448f511102a7c7";
+const char     GENESIS_COINBASE_TX_HEX[]			= "";
 const uint32_t GENESIS_NONCE                         	        = 10000;
 const uint64_t GENESIS_TIMESTAMP				= 1527078920;
 
